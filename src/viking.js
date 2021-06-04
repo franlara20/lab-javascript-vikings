06df6b6934d7868b897bad27d95c45000d815128
Fran Lara <francesc.lara@hotmail.com>
2021-06-04 15:20:41 +0200

    iteration 2 and some 3

--- src/viking.js
+++ src/viking.js
@@ -16,13 +16,63 @@ class Soldier {
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name,health,strength){
+    super(health,strength);
+    this.name = name;
+  }
+
+  receiveDamage(damage){
+    this.health -= damage;
+    if(this.health >= 1){
+      return(`${this.name} has received ${damage} points of damage`);
+    }
+    if(this.health === 0){
+      return(`$(this.name) has died in act of combat`);
+    }
+  }
+
+  battleCry(){
+    return ('Odin Owns You All!');
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  
+  receiveDamage(damage){
+    this.health -= damage;
+
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor(){
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+  addViking(viking){
+
+
+  }
+
+  addSaxon(saxon){
+    
+  }
+
+  vikingAttack(){
+    
+  }
+
+  saxonAttack(){
+    
+  }
+
+  showStatus(){
+    
+  }
+}
 
 
 
